5c617fc19979bf0710c8fe883e2b27f6cfc674a3
Raymond Karstensen <raymond.karstensen@nb.no>
2024-02-28 08:29:53 +0100

    e2e: add e2e tests

--- apps/integration-e2e/src/features/zoom-to-fit.feature
+++ apps/integration-e2e/src/features/zoom-to-fit.feature
@@ -7,7 +7,35 @@ Feature: Zoom to fit current page size in view
   Background:
     Given the viewer is opened with a publication
 
-  Scenario: Fit current page size in dashboard view
-    Given the viewer is in dashboard view
-    When the user navigates between the pages
-    Then the current page's size should be zoomed to fill the viewer
+  # TODO Somehow e2e starts with vertical scroll direction, but view menu shows horizontal direction
+#  Scenario: Fit current page size in dashboard view
+#    Given the viewer is in dashboard view
+#    When the user navigates between the pages
+#    Then the current page's size should be zoomed to fill the viewer
+
+  Scenario Outline: Fit to width and height
+    Given the viewer is in <pageView> view
+    And the layout is <pageLayout>
+    And the scroll direction is <scrollDirection>
+    When the user click the fit to <dimension> button
+    Then the current page size should equal the viewport <dimension>
+    And should update the page size to fit to <dimension> when changing page
+
+    Examples:
+      | pageView  | pageLayout  | scrollDirection | dimension |
+      | page      | two-page    | horizontal      | width     |
+      | page      | two-page    | horizontal      | height    |
+      | page      | two-page    | vertical        | width     |
+      | page      | two-page    | vertical        | height    |
+      | page      | one-page    | horizontal      | width     |
+      | page      | one-page    | horizontal      | height    |
+      | page      | one-page    | vertical        | width     |
+      | page      | one-page    | vertical        | height    |
+      | dashboard | two-page    | horizontal      | width     |
+      | dashboard | two-page    | horizontal      | height    |
+      | dashboard | two-page    | vertical        | width     |
+      | dashboard | two-page    | vertical        | height    |
+      | dashboard | one-page    | horizontal      | width     |
+      | dashboard | one-page    | horizontal      | height    |
+      | dashboard | one-page    | vertical        | width     |
+      | dashboard | one-page    | vertical        | height    |
--- apps/integration-e2e/src/step-definitions/zoom-to-current-page-size.steps.ts
+++ apps/integration-e2e/src/step-definitions/zoom-to-current-page-size.steps.ts
@@ -15,9 +15,52 @@ When(
   }
 );
 
+When('the user click the fit to width button', async function(this: CustomWorld) {
+  if (await this.viewerPage.isPageMode()) {
+    await this.osdToolbarPage.clickFitToWidthButton();
+  } else {
+    await this.viewerHeaderPage.clickFitToWidthButton();
+  }
+});
+
+When('the user click the fit to height button', async function(this: CustomWorld) {
+  if (await this.viewerPage.isPageMode()) {
+    await this.osdToolbarPage.clickFitToHeightButton();
+  } else {
+    await this.viewerHeaderPage.clickFitToHeightButton();
+  }
+});
+
 Then(
   "the current page's size should be zoomed to fill the viewer",
   async function (this: CustomWorld) {
+    await this.animations.waitFor();
     expect(zoomLevels.size).toEqual(2);
   }
 );
+
+Then('the current page size should equal the viewport width', async function(this: CustomWorld) {
+  await this.animations.waitFor();
+  expect(await this.viewerPage.getZoomLevel()).toBeGreaterThan(await this.viewerPage.getHomeZoom());
+  expect(await this.viewerPage.isCurrentCanvasEqualViewportWidth()).toEqual(true);
+});
+
+Then('the current page size should equal the viewport height', async function(this: CustomWorld) {
+  await this.animations.waitFor();
+  expect(await this.viewerPage.getZoomLevel()).toBeGreaterThan(await this.viewerPage.getHomeZoom());
+  expect(await this.viewerPage.isCurrentCanvasEqualViewportHeight()).toEqual(true);
+});
+
+Then('should update the page size to fit to width when changing page', async function(this: CustomWorld) {
+  await this.viewerPage.clickNextButton();
+  await this.animations.waitFor();
+  expect(await this.viewerPage.getZoomLevel()).toBeGreaterThan(await this.viewerPage.getHomeZoom());
+  expect(await this.viewerPage.isCurrentCanvasEqualViewportWidth()).toEqual(true);
+});
+
+Then('should update the page size to fit to height when changing page', async function(this: CustomWorld) {
+  await this.viewerPage.clickNextButton();
+  await this.animations.waitFor();
+  expect(await this.viewerPage.getZoomLevel()).toBeGreaterThan(await this.viewerPage.getHomeZoom());
+  expect(await this.viewerPage.isCurrentCanvasEqualViewportHeight()).toEqual(true);
+});
