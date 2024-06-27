4dc99b564db659094d8f0a33eb80a7b60057887b
Raymond Karstensen <raymond.karstensen@nb.no>
2024-06-27 08:19:06 +0200

    e2e: click FAB button to open OSD toolbar so fit to height and width buttons are clickable

--- apps/integration-e2e/src/pages/osd-toolbar.po.ts
+++ apps/integration-e2e/src/pages/osd-toolbar.po.ts
@@ -2,24 +2,40 @@ import { Locator, Page } from 'playwright';
 
 export class OsdToolbarPage {
   private readonly container: Locator;
+  private readonly fabButton: Locator;
   private readonly fitToWidthButton: Locator;
   private readonly fitToHeightButton: Locator;
 
   constructor(page: Page) {
     this.container = page.locator('.osd-toolbar');
+    this.fabButton = this.container.getByTestId('fabButton');
     this.fitToWidthButton = this.container.getByTestId(
-      'ngx-mimeFitToWidthButton'
+      'ngx-mimeFitToWidthButton',
     );
     this.fitToHeightButton = this.container.getByTestId(
-      'ngx-mimeFitToHeightButton'
+      'ngx-mimeFitToHeightButton',
     );
   }
 
   async clickFitToWidthButton(): Promise<void> {
+    if (!(await this.isFabButtonOpen())) {
+      await this.openFab();
+    }
     await this.fitToWidthButton.click();
   }
 
   async clickFitToHeightButton(): Promise<void> {
+    if (!(await this.isFabButtonOpen())) {
+      await this.openFab();
+    }
     await this.fitToHeightButton.click();
+  }
+
+  private openFab(): Promise<void> {
+    return this.fabButton.click();
+  }
+
+  private async isFabButtonOpen(): Promise<boolean> {
+    return (await this.fabButton.getAttribute('aria-expanded')) === 'true';
   }
 }
