775718d9af7824d800e05258c1815bf66efb29d0
Raymond Karstensen <raymond.karstensen@nb.no>
2024-07-01 12:33:50 +0200

    e2e: change test to check for number of visible canvases

--- apps/integration-e2e/src/step-definitions/layout.steps.ts
+++ apps/integration-e2e/src/step-definitions/layout.steps.ts
@@ -18,34 +18,37 @@ Given('the layout is one-page', async function (this: CustomWorld) {
 
 Then('only the cover page is displayed', async function (this: CustomWorld) {
   await this.animations.waitFor();
-  const visibleCanvasGroups = await this.viewerPage.visibleCanvasGroups();
-  // Firste page visible
+  let visibleCanvasGroups = await this.viewerPage.visibleCanvasGroups();  // Firste page visible
+
   expect(visibleCanvasGroups[0]).toBeTruthy();
   // Rest of the pages should not be visible
-  visibleCanvasGroups.splice(0, 1);
-  expect(visibleCanvasGroups.includes(true)).toBeFalsy();
+  visibleCanvasGroups = visibleCanvasGroups.splice(0, 1);
+
+  expect(visibleCanvasGroups.length).toEqual(1);
 });
 
 Then('page 2 and 3 are displayed', async function (this: CustomWorld) {
   await this.animations.waitFor();
 
-  const visibleCanvasGroups = await this.viewerPage.visibleCanvasGroups();
+  let visibleCanvasGroups = await this.viewerPage.visibleCanvasGroups();
   // Second and third pages visible
   expect(visibleCanvasGroups[1]).toBeTruthy();
   expect(visibleCanvasGroups[2]).toBeTruthy();
 
   // Rest of the pages should not be visible
-  visibleCanvasGroups.splice(1, 2);
-  expect(visibleCanvasGroups.includes(true)).toBeFalsy();
+  visibleCanvasGroups = visibleCanvasGroups.splice(1, 2);
+
+  expect(visibleCanvasGroups.length).toEqual(2);
 });
 
 Then('only page 2 is displayed', async function (this: CustomWorld) {
   await this.animations.waitFor();
-  const visibleCanvasGroups = await this.viewerPage.visibleCanvasGroups();
+  let visibleCanvasGroups = await this.viewerPage.visibleCanvasGroups();
   // Second page visible
   expect(visibleCanvasGroups[1]).toBeTruthy();
 
   // Rest of the pages should not be visible
-  visibleCanvasGroups.splice(1, 1);
-  expect(visibleCanvasGroups.includes(true)).toBeFalsy();
+  visibleCanvasGroups = visibleCanvasGroups.splice(1, 1);
+
+  expect(visibleCanvasGroups.length).toEqual(1);
 });
